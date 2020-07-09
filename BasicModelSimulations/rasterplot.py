e1781f3fbb7cf0f8f5bfbd8dec004b88e164a137
Jorge-Diez <jorge.diez.garcia-victoria@alumnos.upm.es>
2020-07-09 08:10:48 +0200

    remade functionalities in rasterplot, saved bug, vth values now saved. node differentiation complete disaster

--- BasicModelSimulations/rasterplot.py
+++ BasicModelSimulations/rasterplot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import scipy.io as sio
 import numpy as np
 import os
@@ -89,16 +90,23 @@ for skptr in range(1,len(all_spikes_grouped)):
 all_spikes = np.sort(all_spikes)
 minpause = float(input("select min pause in ms to show in rasterplot: "))
 total_pause_count = 0 #coutns how many ms of total pauses
+ax = plt.gca()
 for i in range (len(all_spikes)-1):
      
     if (all_spikes[i+1] - all_spikes[i] >= minpause):
+        
+        """
         #plot the vertical lines between the pause points
         plt.plot( [all_spikes[i], all_spikes[i]], [0, spike_count  ], 'r--')
         plt.plot( [all_spikes[i+1], all_spikes[i+1]], [0, spike_count  ], 'r--')
+        """
         
-        #plot horizontal lines at the top and bottom
-        plt.plot( [all_spikes[i], all_spikes[i+1]], [0, 0  ], 'r')
-        plt.plot( [all_spikes[i], all_spikes[i+1]], [spike_count, spike_count  ], 'r')
+        #plot horizontal lines at the top 
+        #plt.plot( [all_spikes[i], all_spikes[i+1]], [0, 0  ], 'r', linewidth=7.0)
+        rect_length = (all_spikes[i+1] - all_spikes[i]) - 2.5
+        rect_pause = plt.Rectangle((all_spikes[i]+2.5, spike_count+0.5),rect_length, 0.5, color='r')
+        ax.add_patch(rect_pause)
+        #plt.plot( [all_spikes[i]+2, all_spikes[i+1]-2], [spike_count+0.5, spike_count+0.5  ], 'r')
         
         #add pause to total
         total_pause_count += all_spikes[i+1] - all_spikes[i]
@@ -120,9 +128,8 @@ plt.savefig(RESULTS_FOLDER + "\\" + "Spiketrains_sync_pauses",bbox_inches='tight
 
 ############################################################################################################
 ############################################################################################################
+#AMPLITUDE DISTRIBUTION
 
-
-#amplitude distribution
 # 0.01 ms bin 
 t_vec = np.arange(0,1000.01,0.01)
 hist, bins = np.histogram(all_spikes, t_vec)
@@ -145,6 +152,7 @@ plt.savefig(RESULTS_FOLDER + "\\" + "Amplitude distribution",bbox_inches='tight'
 
 ############################################################################################################
 ############################################################################################################
+#AMPLITUDE FREQUENCY DISTRIBUTION
 
 
 bars, bars_counts = np.unique(np.sort(hist), return_counts = True)
@@ -188,33 +196,62 @@ plt.xticks(np.arange(len(bars)), bars)
 
 ############################################################################################################
 ############################################################################################################
+#BINNED AMPLITUDE DISTRIBUTION
 
 np.sort(all_spikes)
 bin = float(input("select bin for amplitude-probability distribution: "))
-counts = []
-for i in np.arange(0,1000,bin):
-    counts.append(len(list(x for x in all_spikes if i <= x <= i+bin)))
-    
 
-bars_bins, bar_bins_counts = np.unique(np.sort(counts), return_counts = True)
-#delete cases for 0
-bars_bins = np.delete(bars_bins, 0)
-bar_bins_counts = np.delete(bar_bins_counts, 0)
-probs_bins = bar_bins_counts/bar_bins_counts.sum()
+
+t_vec = np.arange(0,1000+bin,bin)
+hist, bins = np.histogram(all_spikes, t_vec)
+
+max_bin = max(hist)
+
+
 plt.figure(4, figsize=(10,8))
+plt.hist(hist[hist != 0], range(max_bin+1), ec='black')
+locs, labels = plt.yticks()
 plt.title("Amplitude distribution for bins of " + str(bin) + " ms for " + exp_name)
-plt.bar(np.arange(len(bars_bins)),probs_bins)
 plt.xlabel('Amplitude')
-plt.ylabel('Probability')
-plt.xticks(np.arange(len(bars_bins)), bars_bins)
+plt.ylabel('Nr of repetitions')
 plt.savefig(RESULTS_FOLDER + "\\" + "Amplitude_distribution_bins",bbox_inches='tight')
 
 
 
+############################################################################################################
+############################################################################################################
+#BINNED AMPLITUDE FREQUENCY DISTRIBUTION
+
+
+bars, bars_counts = np.unique(np.sort(hist), return_counts = True)
+#delete cases for 0
+bars_full = np.zeros(max_bin+1)
+
+bars = np.delete(bars, 0)
+bars_counts = np.delete(bars_counts, 0)
+bars_full[bars] = bars_counts
+
+probs_full = bars_full/bars_full.sum()
+
+plt.figure(5, figsize=(10,8))
+plt.title("Amplitude-Frequency distribution for bins of " + str(bin) + " ms for " + exp_name)
+
+plt.bar(np.arange(len(bars_full)),probs_full)
+plt.xlabel('Amplitude')
+plt.ylabel('Probability')
+plt.savefig(RESULTS_FOLDER + "\\" + "Amplitude-Frequency distribution bins",bbox_inches='tight')
+
+
+
+
+
+
+
+
 
 ############################################################################################################
 ############################################################################################################
-#inverting spike binary represantion to show pauses
+#INVERSE AMPLITUDE DISTRIBUTION (PAUSES)
 
 
 inverted_spike_bits = 1 - spike_bits
@@ -233,7 +270,7 @@ hist, bins = np.histogram(all_inverted_spikes, t_vec)
 
 
 
-plt.figure(5, figsize=(10,8))
+plt.figure(6, figsize=(10,8))
 plt.hist(hist[hist != 0], range(30), ec='black')
 locs, labels = plt.yticks()
 plt.title("Amplitude distribution of pauses for " + exp_name)
@@ -246,9 +283,31 @@ plt.savefig(RESULTS_FOLDER + "\\" + "Amplitude distribution pauses without n = 3
 
 ############################################################################################################
 ############################################################################################################
-
+#BINNED INVERSE AMPLITUDE DISTRIBUTION (PAUSES)
 
 bin = float(input("select bin for amplitude-probability distribution of pauses: "))
+
+
+
+
+t_vec = np.arange(0,1000+bin,bin)
+hist, bins = np.histogram(all_inverted_spikes, t_vec)
+
+min_bin = min(hist)
+max_bin = max(hist)
+
+
+
+plt.figure(7, figsize=(10,8))
+plt.hist(hist[hist != 0], range(min_bin,max_bin), ec='black')
+locs, labels = plt.yticks()
+plt.title("Amplitude distribution of pauses for bins of " + str(bin) + " ms for " + exp_name)
+plt.xlabel('Amplitude')
+plt.ylabel('Nr_repetitions')
+plt.savefig(RESULTS_FOLDER + "\\" + "Amplitude_distribution_bins_pauses",bbox_inches='tight')
+
+
+"""
 counts = []
 print("Calculating amplitude-probability distibution for pauses.....")
 iterations = len(np.arange(0,1000,bin))
@@ -262,14 +321,14 @@ bars_bins, bar_bins_counts = np.unique(np.sort(counts), return_counts = True)
 bars_bins = np.delete(bars_bins, 0)
 bar_bins_counts = np.delete(bar_bins_counts, 0)
 probs_bins = bar_bins_counts/bar_bins_counts.sum()
-plt.figure(6, figsize=(19,10))
+plt.figure(7, figsize=(19,10))
 plt.title("Amplitude distribution of pauses for bins of " + str(bin) + " ms for " + exp_name)
 plt.xlabel('Amplitude')
 plt.ylabel('Probability')
 plt.bar(np.arange(len(bars_bins)),probs_bins)
 plt.xticks(np.arange(len(bars_bins)), bars_bins, fontsize=7)
 plt.savefig(RESULTS_FOLDER + "\\" + "Amplitude_distribution_bins_pauses",bbox_inches='tight')
-
+"""
 
 
 
@@ -286,16 +345,24 @@ firing_rate = []
 for i in np.arange(0,1000,firing_rate_bin):
     spike_bits_slice = spike_bits[:,i*100:(i+firing_rate_bin)*100]
     nr_spikes = np.count_nonzero(spike_bits_slice)
-    firing_rate.append(nr_spikes)
+    firing_rate.append(nr_spikes) # NOT IN HZ!
+    
+    
+firing_rate_np = npa = np.asarray(firing_rate, dtype=np.float32)    
+#conversion to hz
+firing_rate = firing_rate_np * (1000/firing_rate_bin)
     
 gauss_filtered = gaussian_filter1d(firing_rate, sigma = 1, order = 0) #linear gaussian filter
 
 
 firing_rate_filtered_savgol = savgol_filter(firing_rate, 31, 3) #savgol filter
 
-plt.figure(7, figsize=(10,8))
+plt.figure(8, figsize=(10,8))
 plt.plot( np.arange(0,1000,firing_rate_bin) , firing_rate_filtered_savgol )
 plt.title("Population firing rate for bins of: "+ str(firing_rate_bin) + " ms for " + exp_name)
+plt.xlabel('Time (ms)')
+plt.ylabel('Firing rate (Hz)')
+plt.savefig(RESULTS_FOLDER + "\\" + "Population firing rate",bbox_inches='tight')
 
 
 
@@ -320,8 +387,8 @@ upper_corr_matrix = np.ma.array(corr_matrix, mask=upper_mask)
 
 
 
-plt.figure(8, figsize=(10,8))
-plt.matshow(upper_corr_matrix, fignum=8)
+plt.figure(9, figsize=(10,8))
+plt.matshow(upper_corr_matrix, fignum=9)
 plt.plot(  [0,30] , [30-nr_neurons-0.5,30-nr_neurons-0.5], 'k'   )
 plt.plot(  [30-nr_neurons-0.5,30-nr_neurons-0.5] , [-0.5,30] , 'k'  )
 
@@ -349,8 +416,8 @@ baseline_group_mean= np.sum(masked_zeroed_corr_matrix)/ np.count_nonzero(masked_
 
 
 #plot and save
-plt.figure(9, figsize=(10,8))
-plt.matshow(upper_corr_matrix, fignum=9)
+plt.figure(10, figsize=(10,8))
+plt.matshow(upper_corr_matrix, fignum=10)
 plt.colorbar()
 plt.title("Correlation matrix of baseline frequency spiketrains for " + exp_name + "\n" + "Mean of baseline freq: " + str(baseline_group_mean))
 
@@ -378,8 +445,8 @@ freqinc_group_mean= np.sum(masked_zeroed_corr_matrix)/ np.count_nonzero(masked_z
 
 
 #plot and save
-plt.figure(10, figsize=(10,8))
-plt.matshow(upper_corr_matrix, fignum=10)
+plt.figure(11, figsize=(10,8))
+plt.matshow(upper_corr_matrix, fignum=11)
 plt.colorbar()
 plt.title("Correlation matrix of frequency increase spiketrains for " + exp_name + "\n" + "Mean of freqinc: " + str(freqinc_group_mean))
 
