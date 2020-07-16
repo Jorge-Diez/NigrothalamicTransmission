ed3d31e409e5611a4f39bb4cd8f77a2466309341
Jorge-Diez <jorge.diez.garcia-victoria@alumnos.upm.es>
2020-07-16 03:27:16 +0200

    added new vth experiments. created tcmodelthrregoup, works on diffmoth and working on deletion method

--- BasicModelSimulations/functions/TCmodel_threegroup.m
+++ BasicModelSimulations/functions/TCmodel_threegroup.m
@@ -1,7 +1,6 @@
-
 % simulation done supposing that there is only 1 core performing
 % each experiment, done for the iceberg HPC
-function [dir_name] = TCmodel_threegroup(job_id, num_jobs)
+function [dir_name] = TCmodel_threegroup
 
 
 
@@ -9,13 +8,11 @@ function [dir_name] = TCmodel_threegroup(job_id, num_jobs)
 
 
 %% Parameter creation and sim mode
-sim_mode = 1;
+sim_mode = 2;
 
 
-F_SNr = 51:1:90; %firing rate increase
-nr_neurons = 1:1:30; %nr of neurons with firing rate increase
-FREQ_NR_COMB = combvec(nr_neurons, F_SNr);
-
+F_SNr = [50 20 82];
+FG_SNR = [3 9 18]; %nr of neurons with firing rate increase
 
 
 
@@ -37,9 +34,6 @@ N_SNr = 30; %number of nigral inputs
 % F_CX = 10*ones(size(T));
 % F_SNr = 80*sigmf(T,[-0.1 mov_onset]);
 
-corr_CX = 0;  %correlation of exc inputs
-corr_SNr = 0; % correlation of nigral inputs
-
 G_SNr_all = 0.70;  %GS (conductances) %CHANGED
 G_SNr = G_SNr_all;
 num_trials = 10;   %NT  THIS IS CHANGED FROM 100 TO 10, SHOULD CHANGE TO 100 FOR GOOD RESULTS (LONG!)
@@ -50,115 +44,72 @@ num_trials = 10;   %NT  THIS IS CHANGED FROM 100 TO 10, SHOULD CHANGE TO 100 FOR
 corr_vals = 0.3:0.1:1;   %values of correlation among inhibitory inputs
 
 NT_GS_JV_TF_all = combvec(G_SNr,corr_vals); %matrix of all possible combinations of conductances and corr values
-ALL_EXPERIMENTS = combvec(NT_GS_JV_TF_all, FREQ_NR_COMB);
 %use number of threads available as MAX
 
-NT_GS_JV_TF = ALL_EXPERIMENTS(:,job_id:num_jobs:end); %parameter space
 
-% comb_trial_num = NT_GS_JV_TF(1,:);
-G_SNr = NT_GS_JV_TF(1,:); %extract conductance value
-jit_val = NT_GS_JV_TF(2,:); %extract correlation value
-nr_neurons_increase = NT_GS_JV_TF(3,:); %extract number of neurons with freq increase
-freq_increase = NT_GS_JV_TF(4,:); % extract the frequency increase value
 
-rebound_spk = zeros(9600/num_jobs,num_trials); %Result vector, rebound spikes after decrease
-all_reb_spk = zeros(9600/num_jobs,num_trials); %Result vector, (check how its done may be the whole simuation)
+rebound_spk = zeros(1,num_trials); %Result vector, rebound spikes after decrease
+all_reb_spk = zeros(1,num_trials); %Result vector, (check how its done may be the whole simuation)
 
-dir_name_list = strings(1, 9600/num_jobs );
 
-parfor exp = 1:size(NT_GS_JV_TF,2)
-    R2 = zeros(size(NT_GS_JV_TF,2),num_trials); %used to store the correlation values
-    
-    
-    [F_SNr, FG_SNR] = par_obtain(nr_neurons_increase(exp), freq_increase(exp), N_SNr);
-    
-    SPK = [];
-    
-    exppath = ['CLUSTERTRY/FREQINCTO_',num2str(freq_increase(exp))];
-    freq_experiment = fullfile(pwd, exppath);
-    if exist(freq_experiment,'dir') ~= 7
-        mkdir(freq_experiment)
-    end
-    
-    specific_folder = ['nr_neurons_',num2str(nr_neurons_increase(exp))];
-    dir_name = fullfile(freq_experiment,specific_folder );
-    
-    dir_name_trace = [dir_name,'/voltage-traces-and-inputs/'];
-    if exist(dir_name_trace,'dir') ~= 7
-        mkdir(dir_name_trace)
-    end
-    
-    
-    
-    %perform experiment
-    
-    
+
+
+
+exppath = ['DELETMETHOD_THREEGROUP_C1'];
+dir_name = fullfile(pwd, exppath);
+if exist(dir_name,'dir') ~= 7
+    mkdir(dir_name)
+end
+
+
+dir_name_trace = [dir_name,'/voltage-traces-and-inputs/'];
+if exist(dir_name_trace,'dir') ~= 7
+    mkdir(dir_name_trace)
+end
+
+
+
+%perform experiment
+
+parfor S = 1:size(NT_GS_JV_TF_all,2)
     switch sim_mode %for now we only have sim mode 1 and 2
         
         case 1
             
-            [rebound_spk(exp,:),all_reb_spk(exp,:)] = ...
+            [rebound_spk(S,:),all_reb_spk(S,:)] = ...
                 TC_model_CX_SNr_cond_changed_parfor_opt_diff_mothspikes(...
-                F_SNr,0,G_SNr(exp),...
-                T,mov_onset,jit_val(exp),...
-                num_trials,dir_name_trace,exp,FG_SNR);
+                F_SNr,0,G_SNr,...
+                T,mov_onset,corr_vals(S),...
+                num_trials,dir_name_trace,S,FG_SNR);
         case 2
             
-            [rebound_spk(exp,:),all_reb_spk(exp,:)] = ...
+            [rebound_spk(S,:),all_reb_spk(S,:)] = ...
                 TC_model_CX_SNr_cond_changed_parfor_opt(...
-                F_SNr,0,G_SNr(exp),...
-                T,mov_onset,jit_val(exp),...
-                num_trials,dir_name_trace,exp,FG_SNR);
+                F_SNr,0,G_SNr,...
+                T,mov_onset,corr_vals(S),...
+                num_trials,dir_name_trace,S,FG_SNR);
             
             
     end
     
-    disp(['subexperiment ', num2str(exp), ' has finished'])
-    
-    
-    
-    
-    
+    disp(['subexperiment ', num2str(S), ' has finished'])
     
 end
 
-% save results
-for i = 1:length(dir_name_list)
-    
-    
-    exppath = ['CLUSTERTRY/FREQINCTO_',num2str(freq_increase(i))];
-    freq_experiment = fullfile(pwd, exppath);
-
-    
-    specific_folder = ['nr_neurons_',num2str(nr_neurons_increase(i))];
-    dir_name = fullfile(freq_experiment,specific_folder );
-    
 
 
-    dir_name_cp = [dir_name,'/res-for-colorplot/'];
-    
-    if exist(dir_name_cp,'dir') ~= 7
-        mkdir(dir_name_cp)
-    end
-    
-    
-    [F_SNr, FG_SNR] = par_obtain(nr_neurons_increase(i), freq_increase(i), N_SNr);
-    
-    temp_rebound_spk = rebound_spk(i,:);
-    temp_all_reb_spk = all_reb_spk(i,:);
-    temp_G_SNR = G_SNr(i);
-    temp_NT_GS_JV_TF = NT_GS_JV_TF(:,i);
-    temp_F_SNr = F_SNr;
-    temp_FG_SNR = FG_SNR;
-    
-    
-    save([dir_name_cp 'corr-' num2str(jit_val(i)*100) '-freq_inc-' num2str(freq_increase(i)) '-nSNr_inc-' num2str(nr_neurons_increase(i)) '-job-' num2str(job_id)],...
-        'temp_rebound_spk','temp_all_reb_spk','temp_G_SNR',...
-        'num_trials','temp_NT_GS_JV_TF','temp_F_SNr','temp_FG_SNR')
-    
-    
-    
+dir_name_cp = [dir_name,'/res-for-colorplot/'];
+
+if exist(dir_name_cp,'dir') ~= 7
+    mkdir(dir_name_cp)
 end
+
+
+save([dir_name_cp '-nSNr-' num2str(sum(FG_SNR))],...
+    'rebound_spk','all_reb_spk','G_SNr',...
+    'num_trials','NT_GS_JV_TF_all','F_SNr','FG_SNR', 'corr_vals')
+
+
 
 
 
